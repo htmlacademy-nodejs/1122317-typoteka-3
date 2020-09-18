c5985e600b81951a05d5a98cea7646ad5198383a
Anastasiya <stacy.salamander@yandex.ru>
2020-09-18 12:21:40 +0300

    add nodemon server

--- src/constants.js
+++ src/constants.js
@@ -23,3 +23,11 @@ module.exports.MONTHS = {
   november: 11,
   december: 12
 };
+
+module.exports.HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
