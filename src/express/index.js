87244c413b49767a736c238924e8c7e3f638ce93
Anastasiya <stacy.salamander@yandex.ru>
2020-09-25 21:36:40 +0300

    add pug templates

--- src/express/index.js
+++ src/express/index.js
@@ -1,13 +1,20 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
+const TEMPLATES_DIR = `templates`;
 const app = express();
 
 const mainRoutes = require(`./routes/main-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const articleRoutes = require(`./routes/article-routes`);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.set(`views`, path.resolve(__dirname, TEMPLATES_DIR));
+app.set(`view engine`, `pug`);
 
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
